f270dfdcf8b3d26a80286d1e84a42ab7519334b5
battis <battis@32a62e83-654b-34ea-056e-6e69a04e166d>
2010-12-01 22:07:37 +0000

    I added a dummy Yadda Yadda class.

---
+++ trunk/HWChecker/Classes/yaddayadda.h
@@ -0,0 +1,16 @@
+//
+//  yaddayadda.h
+//  HWChecker
+//
+//  Created by Seth Battis on 12/1/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface yaddayadda : UIView {
+
+}
+
+@end
