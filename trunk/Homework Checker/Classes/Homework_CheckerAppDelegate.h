96360ce1f5b9eb464fea772b9b91b93f52af8415
battis <battis@32a62e83-654b-34ea-056e-6e69a04e166d>
2010-12-01 17:50:16 +0000

    Initial import of a blank Window-based application template

---
+++ trunk/Homework Checker/Classes/Homework_CheckerAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  Homework_CheckerAppDelegate.h
+//  Homework Checker
+//
+//  Created by Seth Battis on 12/1/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Homework_CheckerAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
